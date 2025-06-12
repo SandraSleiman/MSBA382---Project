12a470f222c481759be11b21f87ed6097ed93f2b
SandraSleiman <sleimannsandra@gmail.com>
2025-06-12 23:47:05 +0300

    fix: change file name

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if not st.session_state.authenticated:
 # --- Load cleaned data ---
 @st.cache_data
 def load_data():
-    df = pd.read_excel("Sleep_Efficiency_Cleaned.xlsx")
+    df = pd.read_excel("Sleep Efficiency.xlsx")
     return df
 
 df = load_data()
