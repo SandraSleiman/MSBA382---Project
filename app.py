d5d098a07f12133ffa28ba6c4407100ea8f72342
SandraSleiman <sleimannsandra@gmail.com>
2025-06-20 20:01:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -117,16 +117,15 @@ if not st.session_state.authenticated:
     
     with col1:
     st.markdown("""
-        <div style='padding-top: 30px; background-color: #111827; color: white; border-radius: 12px; padding: 25px;'>
-            <h1 style='font-size: 42px;'>Sleep Health & Lifestyle Factors</h1>
-            <h3 style='font-size: 24px;'>MSBA 382 — Healthcare Analytics Project</h3>
-            <p style='font-size: 17px; max-width: 600px;'>
-                Welcome to this interactive dashboard exploring how lifestyle habits such as
-                alcohol consumption, caffeine intake, smoking, and physical activity influence
-                sleep efficiency and duration.
-            </p>
-        </div>
+        <h1 style='font-size: 42px; color: white;'>Sleep Health & Lifestyle Factors</h1>
+        <h3 style='font-size: 24px; color: white;'>MSBA 382 — Healthcare Analytics Project</h3>
+        <p style='font-size: 17px; max-width: 600px; color: white;'>
+            Welcome to this interactive dashboard exploring how lifestyle habits such as
+            alcohol consumption, caffeine intake, smoking, and physical activity influence
+            sleep efficiency and duration.
+        </p>
     """, unsafe_allow_html=True)
+
     
     with col2:
         try:
